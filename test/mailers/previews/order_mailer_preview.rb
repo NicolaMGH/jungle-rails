2d92974bc0d4b33d0fff31d630fe48c7725a32d7
Nicola <va3nky@hotmail.com>
2018-10-12 17:56:56 +0000

    updated README

--- test/mailers/previews/order_mailer_preview.rb
+++ test/mailers/previews/order_mailer_preview.rb
@@ -1,6 +1,6 @@
 class OrderMailerPreview < ActionMailer::Preview
   def email_receipt
-    @order = Order.find_by(id: 3)
+    @order = Order.find_by(id: 1)
     OrderMailer.email_receipt(@order)
   end
 end
