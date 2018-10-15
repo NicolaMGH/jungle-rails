14fdf24b0a036494972808c0096833d4feaa003d
Nicola <va3nky@hotmail.com>
2018-10-15 21:20:05 +0000

    added test for add to cart from home page feature

---
+++ spec/features/add_to_carts_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCart", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "Cart Updated By One" do
+    # ACT
+    visit root_path
+
+    first('.product').click_on('Add')
+    sleep(5)
+
+    # VERIFY
+    expect(page).to have_text 'My Cart (1)'
+
+    # DEBUG
+    save_screenshot
+  end
+
+end
